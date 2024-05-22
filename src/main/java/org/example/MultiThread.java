2b41cb81cfab08c700b7ff917a981ddaa2672672
AKO352 <arkodeep.koley@maersk.com>
2024-05-22 11:53:28 +0530

    Started a new Project for learning MultiThreading.

---
+++ src/main/java/org/example/MultiThread.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+//Nothing here just learning about threads.
+public class MultiThread extends Thread{
+private  int threadNum;
+public MultiThread(int threadNum) {
+    this.threadNum = threadNum;
+}
+    public void run(){
+        for (int i=0;i<5;i++){
+            System.out.println(i + " from thread number:- "+threadNum);
+        }
+        try{
+            Thread.sleep(1000);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
